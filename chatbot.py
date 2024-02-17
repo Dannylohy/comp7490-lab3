528fb46815c95f4e4800d1ccdc4cafc4aca187cd
Dannylohy <dannylo0718@gmail.com>
2024-02-17 21:35:59 +0800

    [Description]

--- chatbot.py
+++ chatbot.py
@@ -1,24 +1,74 @@
-import telegram
-from telegram.ext import Updater, MessageHandler, Filters
+from telegram import Update
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, CallbackContext)
 
 import configparser
 import logging
+import redis
+from ChatGPT_HKBU import HKBU_ChatGPT
 
+global redis1
 def main():
 	config = configparser.ConfigParser()
 	config.read('config.ini')
 	updater=Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']),use_context=True)
 	dispatcher=updater.dispatcher
-	logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
-	echo_handler=MessageHandler(Filters.text&(~Filters.command),echo)
-	dispatcher.add_handler(echo_handler)
+	global redis1
+	redis1=redis.Redis(host=(config['REDIS']['HOST']),password=(config['REDIS']['PASSWORD']),port=(config['REDIS']['REDISPORT']))
+
+
+	#logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+	#echo_handler=MessageHandler(Filters.text & (~Filters.command),echo)
+	#dispatcher.add_handler(echo_handler)
+
+	global chatgpt
+	chatgpt = HKBU_ChatGPT(config)
+	chatgpt_handler = MessageHandler(Filters.text & (~Filters.command), equiped_chatgpt)
+	dispatcher.add_handler(chatgpt_handler)
+
+	dispatcher.add_handler(CommandHandler("add",add))
+	dispatcher.add_handler(CommandHandler("hello",hello))
+	dispatcher.add_handler(CommandHandler("help",help_command))
+
 	updater.start_polling()
 	updater.idle()
+
 def echo(update,context):
-	reply_message=update.message.text.upper()
+	reply_message = update.message.text.upper()
 	logging.info("Update: "+str(update))
 	logging.info("context: "+str(context))
-	context.bot.send_message(chat_id=update.effective_chat.id,text=reply_message)
+	context.bot.send_message(chat_id=update.effective_chat.id, text=reply_message)
+
+def equiped_chatgpt(update, context): 
+	global chatgpt
+	reply_message = chatgpt.submit(update.message.text)
+	logging.info("Update: " + str(update))
+	logging.info("context: " + str(context))
+	context.bot.send_message(chat_id=update.effective_chat.id, text=reply_message)
+
+
+def help_command(update: Updater, context: CallbackContext) ->None: 
+	"""Send a message when the command /help is issued."""
+
+	update.message.reply_text('Helping you helping you.')
+
+def hello(update: Updater, context: CallbackContext) ->None: 
+	"""Send a message when the command /hello is issued."""
+
+	update.message.reply_text('Good day, Kevin!')
+
+def add(update: Updater, context: CallbackContext) ->None:
+	"""Send a message when the command /add is issued."""
+	try:
+		global redis1 
+		logging.info(context.args[0])
+		msg=context.args[0]
+		redis1.incr(msg)
+
+		update.message.reply_text('You have said '+msg+' for '+redis1.get(msg).decode('UTF-8')+' times.')
+
+	except (IndexError,ValueError):
+		update.message.reply_text('Usage: /add <keyword>')
+
 
 if __name__=='__main__':
 	main()
